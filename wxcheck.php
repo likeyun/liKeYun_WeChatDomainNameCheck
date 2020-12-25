da71344d7119e0181d4e2ac0e03f7824e0d03868
TANKING <king@likeyunba.com>
2020-12-25 11:37:28 +0800

    Create wxcheck.php

--- wxcheck.php
+++ wxcheck.php
@@ -4,26 +4,32 @@ header('Content-type: application/json;charset=utf-8');
  
 // 官方API接口
 $api = get_headers('http://mp.weixinbridge.com/mp/wapredirect?url='.$_GET['url']);
- 
-// 判断是否被拦截，主要是下标为6的节点返回的是weixin110就代表被封了
-if (trim(empty($_GET['url']))) {
-        $result = array(
-                'code' => 201,
-                'msg' => '请传入需要检测的URL',
-                'api' => '吾爱破解论坛首发'
-        );
-}else if($api[6] !== 'Location: '.$_GET['url'].''){
-        $result = array(
-                'code' => 202,
-                'msg' => '域名被拦截',
-                'api' => '吾爱破解论坛首发'
-        );
-}else{
-        $result = array(
-                'code' => 200,
-                'msg' => '域名正常',
-                'api' => '吾爱破解论坛首发'
-        );
+// 检测url的合法性
+$checkUrl = "/http[s]?:\/\/[\w.]+[\w\/]*[\w.]*\??[\w=&\+\%]*/is";
+
+// 过滤URL
+switch($_GET['url'])
+{
+    case (preg_match($checkUrl,$_GET['url'])):
+    $result = array(
+        'code' => 201,
+        'msg' => '你传入的URL不合法'
+     );
+    break;
+
+    case ($api[6] !== 'Location: '.$_GET['url'].''):
+    $result = array(
+        'code' => 202,
+        'msg' => '域名被拦截'
+     );
+    break;
+
+    case ($api[6] == 'Location: '.$_GET['url'].''):
+    $result = array(
+        'code' => 200,
+        'msg' => '域名正常'
+     );
+    break;
 }
  
 // 输出JSON
