2bce03ae1cc33f39595ea75ddc7f95c5e32137a2
TANKING <king@likeyunba.com>
2022-11-29 12:04:52 +0800

    Update WeChatDomainNameCheck.php

--- WeChatDomainNameCheck.php
+++ WeChatDomainNameCheck.php
@@ -46,6 +46,6 @@ if($Location_behind == 'false'){
 }
 
 // 输出JSON
-echo json_encode($value,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+echo json_encode($result,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
 ?>
